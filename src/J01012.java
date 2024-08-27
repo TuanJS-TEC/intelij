9d548979ccf6ca5496599e824b226020721804b7
TuanJS-TEC <codfish225@gmail.com>
2024-08-27 21:58:21 +0700

    fix bug J01012

--- src/J01012.java
+++ src/J01012.java
@@ -1,21 +1,20 @@
 import java.util.Scanner;
-import java.math.BigInteger;
 
-public class J01012 {
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
+public class J01012{
+    public static void main(String [] args){
+        Scanner sc= new Scanner(System.in);
         int T=sc.nextInt();
         while(T-->0){
-            int n = sc.nextInt();
+            int N = sc.nextInt();
             int cnt=0;
-            for(int i=0;i<=n;i++){
-                if(n%i==0){
+            for(int i=1;i*i<=N;i++){
+                if((N%i)==0){
                     cnt+=(i%2==0?1:0);
-                    if(i!=n/i) cnt+=((i+1)%2==0?1:0);
+                    if(i!=N/i) cnt+=((N/i)%2==0?1:0);
                 }
             }
+            System.out.println(cnt);
         }
         sc.close();
     }
-
 }
